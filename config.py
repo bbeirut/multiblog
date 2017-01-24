229221905361a74fe177499018f8617f1adc7639
willa75 <anthonywi99@yahoo.com>
2017-01-24 18:57:30 -0500

    Added blueprints to project for orginization of view code

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config(object):
-    pass
+    SECRET_KEY = 'ee27ead3f4c0fc52f699379882775d33'
 
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
